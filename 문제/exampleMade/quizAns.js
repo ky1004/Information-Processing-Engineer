589313e1be4f9e71c2bdf51492329f970dac9183
ky1004 <pengukim@naver.com>
2022-04-29 00:06:25 +0900

    Delete file

--- 문제/exampleMade/quizAns.js
+++
@@ -1,9 +0,0 @@
-function Ans1() {
-    // 문제 1
-    document.getElementById('ans1').innerHTML = "ㄴ, ㄱ, ㄷ".fontcolor('red') + '<br>' + "구성/기능/인터페이스 파악".fontcolor('red') + '<br>' + "아키텍처 및  소프트웨어 구성 파악".fontcolor('red') + "<br>" + "하드웨어 및 네트워크 파악".fontcolor('red');
-}
-
-function Ans2() {
-    // 문제 2
-    document.getElementById('ans2').innerHTML = "유스케이스 뷰(Use-Case View)".fontcolor('red') + '<br>' + "논리 뷰(Logical View)".fontcolor('red') + '<br>' + "프로세스 뷰(Process View)".fontcolor('red') + '<br>' + "구현 뷰(Implementation View)".fontcolor('red') + '<br>' + "배포 뷰(Deployment View)".fontcolor('red');
-}
